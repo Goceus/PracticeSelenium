86b950ac7ab763e51217135ae9be6964700333f8
Goce_Cydeo <goce.trpevski@gmail.com>
2023-06-08 15:06:09 -0400

    Finished getDriver method

--- src/test/java/seleniumPractice/utility/WebDriverFactory.java
+++ src/test/java/seleniumPractice/utility/WebDriverFactory.java
@@ -1,4 +1,25 @@
 package seleniumPractice.utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverFactory {
+
+    public static WebDriver getDriver(String arr) {
+        
+        WebDriver driver = null;
+        if (arr.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (arr.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (arr.equalsIgnoreCase("edge") || arr.equalsIgnoreCase("microsoft edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new FirefoxDriver();
+        }
+        return driver;
+    }
 }
